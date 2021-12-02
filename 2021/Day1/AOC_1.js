018788fb42f089751ef233aaa1233e6053dc4e88
cocogelato <33678724+cocogelato@users.noreply.github.com>
2021-12-01 22:53:26 -0800

    Create AOC_1.js
    2021 Day 1 Part 1
    Used JavaScript to solve the Advent Of Code for Day 1 part 1.

---
+++ 2021/Day1/AOC_1.js
@@ -0,0 +1,16 @@
+let curr = 0;
+let prev = 0;
+let count = 0;
+
+var fs = require("fs");
+var text = fs.readFileSync("./myArray.txt").toString('utf-8');
+var arr = text.split("\n");
+
+for(curr; curr < arr.length; curr++) {
+    if(arr[curr] > prev) {
+        count++;
+    }
+    prev = arr[curr];
+}
+
+console.log(count);
